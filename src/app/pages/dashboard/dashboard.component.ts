fd2b2b0b497170a105b4d7ff132bdd4e27d5453a
Classic95 <omkar.kadam.2910@gmail.com>
2021-12-17 00:03:00 +0530

    created dashboard page for user listing

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AgePipe } from 'src/app/pipe/age.pipe';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  userList: any[] = [{
+    name: 'Omkar',
+    email: 'omkar@gmail.com',
+    avatar: 'http://via.placeholder.com/640',
+    address: 'Kashish Park, Thane',
+    dob: 'Thu Dec 16 1995 23:23:19 GMT+0530 (IST)',
+  },
+  {
+    name: 'Omkar Hande',
+    email: 'omkar@hande.com',
+    avatar: 'http://via.placeholder.com/640',
+    address: 'Taloja, Navi Mumbai',
+    dob: 'Thu Dec 17 1996 23:23:19 GMT+0530 (IST)',
+  }]
+
+  constructor(private age: AgePipe) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddEditUserComponent } from './add-edit-user/add-edit-user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgePipe } from '../pipe/age.pipe';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    AddEditUserComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [AgePipe],
+})
+export class PagesModule { }
