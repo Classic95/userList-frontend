bd51b3bad7b6599f3b9bdf16d5294045068ec9d5
Omkar Kadam <itsoftware3@stulzservice.in>
2021-12-18 17:48:52 +0530

    added primeng

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,8 +5,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AgePipe } from '../pipe/age.pipe';
 import { UserCardComponent } from './user-card/user-card.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from '../auth.interceptor';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [
@@ -19,9 +18,10 @@ import { AuthInterceptor } from '../auth.interceptor';
     PagesRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    ChartModule
   ],
   exports: [UserCardComponent],
   providers: [AgePipe],
-  
+
 })
 export class PagesModule { }
